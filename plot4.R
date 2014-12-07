9e6e31ff61c7cf5fba7ea69f49d1b399c30f8ff3
NollAl <alexander.noll@hotmail.com>
2014-12-07 19:06:21 +0100

    Plot 4 corrected
    Plot 4 corrected

--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ plot(datetime,as.numeric(data$Voltage),xlab="datetime",ylab="Voltage",type="l")
 plot(datetime,as.numeric(data$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering")
 points(datetime,as.numeric(data$Sub_metering_2),type="l",col="red" )
 points(datetime,as.numeric(data$Sub_metering_3),type="l",col="purple")
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","purple"),cex=.4)
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","purple"),cex=.5,bty="n")
 
 plot(datetime,data$Global_reactive_power,type="l",ylab="Global_reactive_power")
 
